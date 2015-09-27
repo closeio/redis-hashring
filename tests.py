14ba0df523baa505d76bba66c281aa83390ad27b
Marcus Smith <qwcode@gmail.com>
2015-09-27 12:04:26 -0700

    - add test that uses fakeredis (vs real redis) - modify tests to be more "pytest standard"

--- tests.py
+++ tests.py
@@ -1,45 +1,59 @@
 import unittest
 
+from mock import Mock
 import pytest
 from redis import Redis
+from fakeredis import FakeStrictRedis
+
 
 from redis_hashring import RingNode
 
 TEST_KEY = 'hashring-test'
 
 
-@pytest.mark.requires_redis_server
-class HashRingTestCase(unittest.TestCase):
+class TestHashRingFunctional(object):
 
-    def setUp(self):
-        self.redis = Redis()
+    def tearDown(self):
         self.redis.delete(TEST_KEY)
 
     def get_node(self, n_replicas, total_replicas):
         node = RingNode(self.redis, TEST_KEY, n_replicas=n_replicas)
 
-        self.assertEqual(len(node.replicas), n_replicas)
-        self.assertEqual(self.redis.zcard(TEST_KEY), total_replicas-n_replicas)
+        assert len(node.replicas) == n_replicas
+        assert self.redis.zcard(TEST_KEY) == total_replicas-n_replicas
 
         node.heartbeat()
 
-        self.assertEqual(self.redis.zcard(TEST_KEY), total_replicas)
-        self.assertEqual(len(node.ranges), 0)
+        assert self.redis.zcard(TEST_KEY) == total_replicas
+        assert len(node.ranges) == 0
 
         return node
 
-    def test_node(self):
+    def ring_assertions(self):
         node1 = self.get_node(1, 1)
         node1.update()
-        self.assertEqual(len(node1.ranges), 1)
+        assert len(node1.ranges) == 1
 
         node2 = self.get_node(1, 2)
         node1.update()
         node2.update()
-        self.assertEqual(len(node1.ranges) + len(node2.ranges), 3)
+        assert len(node1.ranges) + len(node2.ranges) == 3
 
         node3 = self.get_node(2, 4)
         node1.update()
         node2.update()
         node3.update()
-        self.assertEqual(len(node1.ranges) + len(node2.ranges) + len(node3.ranges), 5)
+        assert len(node1.ranges) + len(node2.ranges) + len(node3.ranges) == 5
+
+    @pytest.mark.requires_redis_server
+    def test_with_redis_server(self):
+        self.redis = Redis()
+        self.ring_assertions()
+
+    def test_with_fake_redis_server(self, monkeypatch):
+        self.redis = FakeStrictRedis()
+
+        # mock publish, since fakeredis doesn't support
+        self.redis.publish = Mock()
+
+        self.ring_assertions()
