4c541e8b84838113110e90a95c243fd92bcba329
Thomas Steinacher <tom@eggdrop.ch>
2016-09-16 15:33:17 -0700

    Fix redis_hashring distribution

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='redis-hashring',
-    version='0.1',
+    version='0.1.1',
     url='http://github.com/closeio/redis-hashring',
     license='MIT',
     description='Python library for distributed applications using a Redis hash ring',
@@ -17,5 +17,8 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
+    ],
+    packages=[
+        'redis_hashring',
     ],
 )
