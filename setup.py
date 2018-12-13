51d169697c840afa2638d92f29e322e4edec0892
Joe Kemp <joe@six7.io>
2018-12-13 16:24:27 -0500

    Pin redis package to 2.10.6 (#7)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     license='MIT',
     description='Python library for distributed applications using a Redis hash ring',
     test_suite='tests',
-    tests_require=['redis'],
+    tests_require=['redis==2.10.6'],
     platforms='any',
     classifiers=[
         'Intended Audience :: Developers',
