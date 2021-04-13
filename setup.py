52629fc84234bc261e0e789f0b5b8a92504dea9d
João Sampaio <joao.sampaio@close.com>
2021-04-13 14:42:04 -0300

    v0.3.1 (#15)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='redis-hashring',
-    version='0.3.0',
+    version='0.3.1',
     author='Close Engineering',
     author_email='engineering@close.com',
     url='http://github.com/closeio/redis-hashring',
