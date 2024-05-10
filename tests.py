0afe1887e95b5e0c0b4640ef5702e944d6a2697a
João Sampaio <joao.sampaio@close.com>
2024-05-10 10:46:33 +0100

    API to get node count in ring and ranges for node (#25)

--- tests.py
+++ tests.py
@@ -1,3 +1,6 @@
+import socket
+from unittest.mock import patch
+
 import pytest
 from redis import Redis
 
@@ -16,29 +19,49 @@ def redis():
 def get_node(redis, n_replicas, total_replicas):
     node = RingNode(redis, TEST_KEY, n_replicas=n_replicas)
 
-    assert len(node.replicas) == n_replicas
+    assert len(node._replicas) == n_replicas
     assert redis.zcard(TEST_KEY) == total_replicas - n_replicas
 
     node.heartbeat()
 
     assert redis.zcard(TEST_KEY) == total_replicas
-    assert len(node.ranges) == 0
+    assert len(node.get_ranges()) == 0
 
     return node
 
 
 def test_node(redis):
-    node1 = get_node(redis, 1, 1)
+    with patch.object(socket, "gethostname", return_value="host1"):
+        node1 = get_node(redis, 1, 1)
     node1.update()
-    assert len(node1.ranges) == 1
+    assert len(node1.get_ranges()) == 1
+    assert node1.get_node_count() == 1
 
-    node2 = get_node(redis, 1, 2)
+    with patch.object(socket, "gethostname", return_value="host2"):
+        node2 = get_node(redis, 1, 2)
     node1.update()
     node2.update()
-    assert len(node1.ranges) + len(node2.ranges) == 3
+    assert len(node1.get_ranges()) + len(node2.get_ranges()) == 3
+    assert node1.get_node_count() == 2
+    assert node2.get_node_count() == 2
 
-    node3 = get_node(redis, 2, 4)
+    with patch.object(socket, "gethostname", return_value="host3"):
+        node3 = get_node(redis, 2, 4)
     node1.update()
     node2.update()
     node3.update()
-    assert len(node1.ranges) + len(node2.ranges) + len(node3.ranges) == 5
+    assert (
+        len(node1.get_ranges())
+        + len(node2.get_ranges())
+        + len(node3.get_ranges())
+        == 5
+    )
+    assert node1.get_node_count() == 3
+    assert node2.get_node_count() == 3
+    assert node3.get_node_count() == 3
+
+
+def test_contains(redis):
+    node1 = get_node(redis, 1, 1)
+    node1.update()
+    assert node1.contains("item") is True
