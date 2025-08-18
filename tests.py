8dc1f77ff79b3200b28270f092ebe2b73645acb2
João Sampaio <joao.sampaio@close.com>
2025-08-18 14:18:36 -0400

    Make algorithm mandatory in test function

--- tests.py
+++ tests.py
@@ -16,10 +16,10 @@ def redis():
     redis.delete(TEST_KEY)
 
 
-def get_node(
-    redis, n_replicas, total_replicas, hash_algorithm=HashAlgorithm.XXHASH
-):
-    node = RingNode(redis, TEST_KEY, n_replicas=n_replicas)
+def get_node(redis, n_replicas, total_replicas, hash_algorithm):
+    node = RingNode(
+        redis, TEST_KEY, n_replicas=n_replicas, hash_algorithm=hash_algorithm
+    )
 
     assert len(node._replicas) == n_replicas
     assert redis.zcard(TEST_KEY) == total_replicas - n_replicas
@@ -34,13 +34,13 @@ def get_node(
 
 def test_node(redis):
     with patch.object(socket, "gethostname", return_value="host1"):
-        node1 = get_node(redis, 1, 1)
+        node1 = get_node(redis, 1, 1, HashAlgorithm.XXHASH)
     node1.update()
     assert len(node1.get_ranges()) == 1
     assert node1.get_node_count() == 1
 
     with patch.object(socket, "gethostname", return_value="host2"):
-        node2 = get_node(redis, 1, 2)
+        node2 = get_node(redis, 1, 2, HashAlgorithm.XXHASH)
     node1.update()
     node2.update()
     assert len(node1.get_ranges()) + len(node2.get_ranges()) == 3
@@ -48,7 +48,7 @@ def test_node(redis):
     assert node2.get_node_count() == 2
 
     with patch.object(socket, "gethostname", return_value="host3"):
-        node3 = get_node(redis, 2, 4)
+        node3 = get_node(redis, 2, 4, HashAlgorithm.XXHASH)
     node1.update()
     node2.update()
     node3.update()
